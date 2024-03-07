6f75ed31d97c4b878ddc4e651227e070639d4b4a
supersujin <sujinjin026@gmail.com>
2024-03-07 21:30:01 +0900

    관심사 설정 자바, xml 기초 구성 및 마이페이지 레이아웃 재설정

--- app/src/main/java/com/example/newkey/ChooseTopicsActivity.java
+++ app/src/main/java/com/example/newkey/ChooseTopicsActivity.java
@@ -6,17 +6,38 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ChooseTopicsActivity extends AppCompatActivity {
 
+
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> myDataset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_topics);
 
-        LinearLayout backFromScrap = findViewById(R.id.back_from_history);
+        LinearLayout backFromTopic = findViewById(R.id.back_from_topic);
 
-        backFromScrap.setOnClickListener(new View.OnClickListener() {
+
+        recyclerView = findViewById(R.id.categoriesListView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        myDataset = new ArrayList<>();
+        // 예제 데이터를 리스트에 추가합니다.
+        myDataset.add("아이템 1");
+        myDataset.add("아이템 2");
+
+        adapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(adapter);
+        backFromTopic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // MainActivity로 돌아가기
