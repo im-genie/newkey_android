ff64a426c6db042587b865023dc2daf8be4da729
Yesl Kwon <yesl0426@naver.com>
2024-03-15 01:16:45 +0900

    second (because of mainactivity) -yesl

--- app/src/main/java/com/example/newkey/MainActivity.java
+++ app/src/main/java/com/example/newkey/MainActivity.java
@@ -7,11 +7,13 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         main_activity_linearlayout1_imageview2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                Intent intent = new Intent(MainActivity.this, search1.class);
                 startActivity(intent);
             }
         });
@@ -38,53 +40,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ImageView main_activity_linearlayout1_imageview1=findViewById(R.id.main_activity_linearlayout1_imageview1);
-
-        main_activity_linearlayout1_imageview1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        ImageView button_home = findViewById(R.id.button_home);
-        ImageView button_feed = findViewById(R.id.button_feed);
-        ImageView button_person = findViewById(R.id.button_person);
-
         // 네비게이션 바: Home
-
-        button_home.setOnClickListener(new View.OnClickListener() {
+        ImageView main_activity_linearlayout2_imageview1_imageview1 = findViewById(R.id.main_activity_linearlayout2_imageview1_imageview1);
+        main_activity_linearlayout2_imageview1_imageview1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
-                button_home.setImageResource(R.drawable.home_green);
-                button_feed.setImageResource(R.drawable.feed);
-                button_person.setImageResource(R.drawable.person);
             }
         });
-
         // 네비게이션 바: news
-        button_feed.setOnClickListener(new View.OnClickListener() {
+        ImageView main_activity_linearlayout2_imageview1_imageview2 = findViewById(R.id.main_activity_linearlayout2_imageview1_imageview2);
+        main_activity_linearlayout2_imageview1_imageview2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, news1_activity.class);
                 startActivity(intent);
-                button_home.setImageResource(R.drawable.home);
-                button_feed.setImageResource(R.drawable.feed_green);
-                button_person.setImageResource(R.drawable.person);
             }
         });
         // 네비게이션 바: mypage
-        button_person.setOnClickListener(new View.OnClickListener() {
+        ImageView main_activity_linearlayout2_imageview1_imageview3 = findViewById(R.id.main_activity_linearlayout2_imageview1_imageview3);
+        main_activity_linearlayout2_imageview1_imageview3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MypageActivity.class);
                 startActivity(intent);
-                button_home.setImageResource(R.drawable.home);
-                button_feed.setImageResource(R.drawable.feed);
-                button_person.setImageResource(R.drawable.person_green);
             }
         });
 
@@ -97,18 +77,6 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ImageView button_home = findViewById(R.id.button_home);
-        ImageView button_feed = findViewById(R.id.button_feed);
-        ImageView button_person = findViewById(R.id.button_person);
-
-        button_home.setImageResource(R.drawable.home_green);
-        button_feed.setImageResource(R.drawable.feed);
-        button_person.setImageResource(R.drawable.person);
     }
 
     @Override
