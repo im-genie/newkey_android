d5f4700ee3e38bc8455c92e0ab4b1f241ba51cb8
Yesl Kwon <yesl0426@naver.com>
2024-03-15 01:16:45 +0900

    rebasing : second -yesl

---
+++ app/src/main/java/com/example/newkey/RecentSearchAdapter.java
@@ -0,0 +1,59 @@
+package com.example.newkey;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecentSearchAdapter extends RecyclerView.Adapter<RecentSearchAdapter.ViewHolder> {
+
+    private List<String> recentSearchList;
+
+    // 데이터 리스트를 여기에 추가해야 합니다.
+    public RecentSearchAdapter(List<String> recentSearchList) {
+        this.recentSearchList = recentSearchList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recentsearch, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String searchItem = recentSearchList.get(position);
+        holder.textSearch.setText(searchItem);
+
+        // 삭제 버튼 클릭 이벤트 처리
+        holder.buttonDelete.setOnClickListener(v -> {
+            // 해당 검색어 삭제 로직 추가
+            recentSearchList.remove(position);
+            notifyDataSetChanged();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return recentSearchList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textSearch;
+        ImageButton buttonDelete;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textSearch = itemView.findViewById(R.id.textSearch);
+            buttonDelete = itemView.findViewById(R.id.buttonDelete);
+        }
+    }
+}
+
