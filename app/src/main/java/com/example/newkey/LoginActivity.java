f23a57ab6d145f30962fae0354a2d7e23d2b4dfa
bce5180 <108451207+bce5180@users.noreply.github.com>
2024-10-01 01:23:51 +0900

    다크모드 여부에 따른 spinner 텍스트 color 변환

--- app/src/main/java/com/example/newkey/LoginActivity.java
+++ app/src/main/java/com/example/newkey/LoginActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -65,7 +66,6 @@ public class LoginActivity extends AppCompatActivity {
 
         // ArrayAdapter를 사용하여 Spinner에 항목 연결
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items) {
-
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 // 기본 레이아웃을 사용하여 뷰 생성
@@ -77,19 +77,27 @@ public class LoginActivity extends AppCompatActivity {
 
                 return view;
             }
-
             @Override
             public View getDropDownView(int position, View convertView, ViewGroup parent) {
                 // 기본 드롭다운 레이아웃을 사용하여 뷰 생성
                 View view = super.getDropDownView(position, convertView, parent);
 
-                // 드롭다운 항목의 텍스트 색상 설정 (gray_100으로 변경)
+                // 현재 모드 감지 (다크모드인지 화이트모드인지)
+                int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+                // 드롭다운 항목의 텍스트 색상 설정 (다크모드일 경우 흰색, 라이트모드일 경우 검정색)
                 TextView text = (TextView) view;
-                text.setTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.black)); // **드롭다운 텍스트는 검정색**
+                if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+                    text.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.gray_100));
+                } else {
+                    text.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.gray_600));
+                }
 
                 return view;
             }
         };
+
+        // Spinner에 어댑터 설정
         email2.setAdapter(adapter);
 
         // Spinner의 항목이 선택되었을 때의 동작 설정
